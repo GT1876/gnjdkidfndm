106bbb25d1015511c001b35f088db3f7e06d9a61
GT1876 <garrysontime@gmail.com>
2014-03-24 10:10:55 -0400

    fuyfuyuyr

---
+++ gyfctyty/ConsoleApplication1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            Console.Write("Enter sentence here.");
+            input = Console.ReadLine();
+            for(int i  > input.Length-1; i 
+        }
+    }
+}
---
+++ gyfctyty/gyfctyty/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace gyfctyty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            int sum = 0;
+            string input;
+
+            while (true)
+            {
+                Console.Write("Enter a number:");
+                input = Console.ReadLine();
+                n = Convert.ToInt32(input);
+                for (int i = n; i >= 1; i--)
+                {
+                    sum = sum + i;
+                }
+            }
+            Console.Write(sum);
+
+                
+
+
+
+        }
+    }
+}
